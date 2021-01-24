1fa153fcd04d0b92fadeb53dc296e2dd55e0416b
LujiaYang1 <76014663+LujiaYang1@users.noreply.github.com>
2021-01-24 11:44:19 -0500

    Add files via upload

---
+++ Election_Analysis/Python_practice.py
@@ -0,0 +1,12 @@
+
+
+print("hello")
+
+import datetime
+
+now= datetime.datetime.now()
+print("The time right now is",now)
+
+import csv
+dir(csv)
+dir({"Arapahoe":422829,"Denver":463353,"Jefferson":432438})
